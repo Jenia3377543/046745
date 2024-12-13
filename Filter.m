ca7884ac8523d980902234823217562077f73a12
Jenia3377543 <3377543@gmail.com>
2024-12-13 17:08:15 +0200

    Implemented filter function. Have to implement using matrix operations

--- Filter.m
+++ Filter.m
@@ -1,3 +1,22 @@
 function [y_n] = Filter(an,bn,x)
-y_n = 0;
+p = length(an);
+bn = bn / an(1);
+an = an / an(1);
+
+if isscalar(an)
+    y_n = conv(x, bn, 'valid');
+    return;
+end
+w_n = conv(x, bn, 'full');
+
+N = length(x);
+
+w_n = cat(2, w_n, zeros(1, 2 * N - length(w_n)));
+y_n = zeros(1, 2 * N);
+
+y_n(1) = w_n(1);
+for j = 2:length(y_n)
+
+    y_n(j) = (-an(2:min(j,p))) * flip(y_n(max(1,j+1-p):j-1))' + w_n(j);
+end
 end
