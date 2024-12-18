5f8065295093fdadf57bc9dac4a4ea7c60744ab8
Jenia3377543 <3377543@gmail.com>
2024-12-18 16:09:17 +0200

    Implemented decimator

--- Decimate.m
+++ Decimate.m
@@ -1,3 +1,8 @@
 function [y_n] = Decimate(D,x)
-y_n = 0;
+% Apply low pass filter
+N_samples = length(x);
+sampled_sinc = sinc((-N_samples/2:N_samples/2)/(D))/D;
+x_low_passed = conv(x, sampled_sinc, 'same');
+
+y_n = x_low_passed(1:D:end);
 end
