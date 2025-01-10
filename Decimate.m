03bdc2ca5e29305989dcaa3ede725c1fb30a9147
Jenia3377543 <3377543@gmail.com>
2025-01-10 16:48:18 +0200

    Fixing decimation and interpolation implementation - making optional LPF

--- Decimate.m
+++ Decimate.m
@@ -1,8 +1,19 @@
-function [y_n] = Decimate(D,x)
+function [y_n] = Decimate(D,x,use_lpf)
+arguments
+    D 
+    x 
+    use_lpf = false
+end
+
 % Apply low pass filter
 N_samples = length(x);
-sampled_sinc = sinc((-N_samples/2:N_samples/2)/(D))/D;
-x_low_passed = conv(x, sampled_sinc, 'same');
+if use_lpf
+    sampled_sinc = sinc((-N_samples/2:N_samples/2)/(D))/D;
+    x_low_passed = conv(x, sampled_sinc, 'same');
+    w_n = x_low_passed;
+else
+    w_n = x;
+end
 
-y_n = x_low_passed(1:D:end);
+y_n = w_n(1:D:end);
 end
--- Interpolate.m
+++ Interpolate.m
@@ -1,12 +1,21 @@
-function [y_n] = Interpolate(M,x)
+function [y_n] = Interpolate(M,x,use_lpf)
+arguments
+    M 
+    x 
+    use_lpf = false
+end
 % Validate that x is row vector
 x = x(:)';
 N_samples = length(x);
 
 y_n = cat(1, x, zeros(M - 1, N_samples));
 y_n = reshape(y_n, 1, []);
-N_samples_interp = N_samples * M;
-% Apply lowpass filter
-sampled_sinc = sinc((-N_samples_interp/2:N_samples_interp/2)/(M));
-y_n = conv(y_n, sampled_sinc, 'same');
+
+if use_lpf
+    % Apply lowpass filter
+    N_samples_interp = N_samples * M;
+    sampled_sinc = sinc((-N_samples_interp/2:N_samples_interp/2)/(M));
+    y_n = conv(y_n, sampled_sinc, 'same');
+end
+
 end
